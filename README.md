82c879c72915dd7e731783a84ed0cd012b648aed
Davide-Marchese <6190189@itisrossi.vi.it>
2026-02-21 22:42:12 +0100

    big leap

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # material_recognition_project
-in ordine di analisi:
+(ldr & resistore 1k in pulldown)
+
+test:
 
 induttivo:
 carta & plastica & vetro: analog = 1
@@ -8,16 +10,28 @@ metallo: analog = 0
 
 laser:
 carta: analog <= 50
-plastica: 80 < analog <= 190
-vetro: 230 < analog < 800
-nulla: analog > 900
+plastica: 80 < analog <= 210
+vetro: 210 < analog < 850
+nulla: analog > 850
 
 
 infrarosso:
 carta: 0 < analog < 40
 plastica: 40 <= analog < 200
-vetro & nulla: analog > 650
+vetro & nulla: analog > 500
 
 
 
-(ldr & resistore 1k in pulldown)
+decisioni in ordine:
+
+se ottico > 850 -> non c'è nulla
+
+se induttivo è falso -> è metallo
+
+se induttivo è vero:
+
+se ottico <= 50 & infrarosso < 40 -> è carta
+
+se infrarosso in [40, 200) & ottico in (80, 210] -> è plastica
+
+se ottico in (210, 850) & infrarosso > 500 -> è vetro
--- material_recognition_project.ino
+++ material_recognition_project.ino
@@ -1,19 +1,109 @@
+#define IND 2
+#define PROX 3
 #define LASER 7
 #define LDR A7
 #define IR A0
-#define IND 2
-void setup() {
-  pinMode(LASER, OUTPUT);
-  Serial.begin(9600);
+#define LCD_ADDR 0x27
+
+#include <LiquidCrystal_I2C.h>
+
+
+LiquidCrystal_I2C lcd(LCD_ADDR, 16, 2);
+
+unsigned short opt_sig = 0;
+unsigned short ir_sig = 0;
+bool ind_sig = 0;
+
+unsigned int usages = 0;
+char materials[][11] = {
+  "nulla",
+  "metallo",
+  "carta",
+  "plastica",
+  "vetro",
+  "indefinito"
+};
+
+void waiting() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("presentare");
+  lcd.setCursor(0, 1);
+  lcd.print("oggetto");
+  while(digitalRead(PROX) == HIGH);
 }
 
+void LCD_init() {
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+}
+
+int decision(unsigned short infrared, unsigned short optical, bool inductive) {
+  // se ottico > 850 -> non c'è nulla
+
+  // se induttivo è falso -> è metallo
+
+  // se induttivo è vero:
+
+  // se ottico <= 50 & infrarosso < 40 -> è carta
+
+  // se infrarosso in [40, 200) & ottico in (80, 210] -> è plastica
+
+  // se ottico in (210, 850) & infrarosso > 500 -> è vetro
+
+  if(optical > 850) return 0;
+  else if(!inductive) return 1;
+  else if(optical <= 50 && infrared < 40) return 2;
+  else if(optical > 80 && optical <= 230 && infrared >= 40 && infrared <= 200) return 3;
+  else if(optical > 230 && optical < 850 && infrared > 500) return 4;
+  else return 5;
+}
+
+void setup() {
+  pinMode(LASER, OUTPUT);
+  pinMode(IND, INPUT);
+  pinMode(PROX, INPUT);
+  Serial.begin(9600);
+  LCD_init();
+}
+
+// void loop() {
+//   digitalWrite(LASER, HIGH);
+// }
+
 void loop() {
-    digitalWrite(LASER, LOW);
-    Serial.print("LDR: ");
-    Serial.print(analogRead(LDR));
-    Serial.print("\tIR: ");
-    Serial.print(analogRead(IR));
-    Serial.print("\tIND: ");
-    Serial.println(digitalRead(IND));
+    waiting();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("inserire oggetto");
+    delay(10000);
+
+    ir_sig = analogRead(IR);
+    delay(100);
+    digitalWrite(LASER, HIGH);
     delay(1000);
+    opt_sig = analogRead(LDR);
+    delay(100);
+    digitalWrite(LASER, LOW);
+    ind_sig = digitalRead(IND);
+
+    int dec = decision(ir_sig, opt_sig, ind_sig);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("riconosciuto:");
+    lcd.setCursor(0, 1);
+    lcd.print(materials[dec]);
+
+    Serial.print("LDR: ");
+    Serial.print(opt_sig);
+    Serial.print("\tIR: ");
+    Serial.print(ir_sig);
+    Serial.print("\tIND: ");
+    Serial.print(ind_sig);
+    Serial.print("\tclass: ");
+    Serial.println(materials[dec]);
+    delay(10000);
 }
