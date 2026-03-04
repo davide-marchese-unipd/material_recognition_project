20edac6cbf412d3e312fa60d4cc6eba8ef2ed36f
Davide-Marchese <6190189@itisrossi.vi.it>
2026-03-04 13:03:44 +0100

    rimozione movimento servo per materiale non identificato

--- material_recognition_project.ino
+++ material_recognition_project.ino
@@ -123,6 +123,7 @@ void servo_reset() {
   * \param recognized_material materiale riconosciuto dal classificatore
 */
 void servo_move(int recognized_material) {
+  if(recognized_material == 5) return;
   for(int i = 90; i >= 0; i--) {
     d_servo.write(i);
     delay(10);
